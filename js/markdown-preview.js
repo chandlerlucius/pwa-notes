e613d601661e05e24f6f6f476382f77838e1dbad
Chandler Lucius <clucius@chandlerlucius.com>
2020-12-20 17:02:44 -0600

    Update markdown-preview.js

--- js/markdown-preview.js
+++ js/markdown-preview.js
@@ -1,22 +1,14 @@
-const convertLineToMarkdown = function(line) {
-    const reader = new commonmark.Parser();
-    const writer = new commonmark.HtmlRenderer();
-    const parsed = reader.parse(this.value);
-    const result = writer.render(parsed);
-    
-    const preview = document.querySelector('#add-note-preview');
-    preview.insertAdjacentHTML('beforeend', result);
-};
+const handleMarkdownPreview = function() {
+    const textarea = document.querySelector('#add-note-textarea');
+    textarea.addEventListener('input', function(line) {
+        const reader = new commonmark.Parser();
+        const writer = new commonmark.HtmlRenderer();
+        const parsed = reader.parse(this.value);
+        const result = writer.render(parsed);
 
-const updatePreview = function() {
-    const preview = document.querySelector('#add-note-preview');
-    const lines = this.value.split('\n');
-    lines.forEach(function(line) {
-        convertLineToMarkdown(line);
+        const preview = document.querySelector('#add-note-preview');
+        preview.insertAdjacentHTML('beforeend', result);
     });
 };
 
-document.addEventListener('DOMContentLoaded', function() {
-    const textarea = document.querySelector('#add-note-textarea');
-    textarea.addEventListener('input', updatePreview);
-});
+document.addEventListener('DOMContentLoaded', handleMarkdownPreview);
