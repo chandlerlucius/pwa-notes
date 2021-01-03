d6fda0f22f58b1a30011b768c113cc9f38c5a4f6
Chandler Lucius <clucius@chandlerlucius.com>
2021-01-02 21:49:29 -0600

    Added constants class

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+
+export default class Constants {
+    static PWA_NOTES_DB = 'PWANotes';
+    static NOTE_OBJECT_STORE = 'note';
+    static NOTE_KEY_SESSION_STORAGE = 'note-key';
+    static NOTE_DATA_ID = '#note-data';
+    static NOTE_TITLE_ID = '#note-title';
+}
