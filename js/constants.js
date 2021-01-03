c56bab27753c5e1fd4e5a3798105768bc2429b0f
Chandler Lucius <clucius@chandlerlucius.com>
2021-01-03 13:26:31 -0600

    Added created index to constants

--- js/constants.js
+++ js/constants.js
@@ -2,6 +2,8 @@
 export default class Constants {
     static PWA_NOTES_DB = 'PWANotes';
     static NOTE_OBJECT_STORE = 'note';
+    static CREATED_INDEX = 'created';
+    
     static NOTE_KEY_SESSION_STORAGE = 'note-key';
     static NOTE_DATA_ID = '#note-data';
     static NOTE_TITLE_ID = '#note-title';
