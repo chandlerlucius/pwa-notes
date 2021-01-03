f41e0afc187032cb2a3f2fc21c3fea1f1fa9250d
Chandler Lucius <clucius@chandlerlucius.com>
2021-01-02 21:50:36 -0600

    Updated preview js to use constants

--- js/markdown-preview.js
+++ js/markdown-preview.js
@@ -1,12 +1,14 @@
+import Constants from './constants.js'
+
 const handleMarkdownPreview = function() {
-    const textarea = document.querySelector('#add-note-textarea');
+    const textarea = document.querySelector(Constants.NOTE_DATA_ID);
     textarea.addEventListener('input', function(line) {
         const reader = new commonmark.Parser();
         const writer = new commonmark.HtmlRenderer();
         const parsed = reader.parse(this.value);
         const result = writer.render(parsed);
 
-        const preview = document.querySelector('#add-note-preview');
+        const preview = document.querySelector('#note-preview');
         preview.innerHTML = '';
         preview.insertAdjacentHTML('beforeend', result);
     });
